2ba4b1ed628543a17a76533bf2e017301c8015fb
Bogdan Bulgakov <Bog.bulgakov@yandex.ru>
2023-12-09 18:02:41 +0300

    готово

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -1,9 +1,25 @@
 package app.config;
 
+
+import app.model.*;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = "app")
 public class AppConfig {
+    @Bean(name="cat")
+    public Cat getCat() {
+        return new Cat();
+    }
+    @Bean(name="dog")
+    public Dog getDog() {
+        return new Dog();
+    }
+    @Bean(name="timer")
+    public Timer getTimer() {
+        return new Timer();
+    }
+
 }
